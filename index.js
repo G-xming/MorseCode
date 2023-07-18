a472d94e7709092041ba2306bb73f28eced6adbf
unknown <SoftysGitHub@proton.me>
2023-07-18 00:22:17 -0500

    d

--- index.js
+++ index.js
@@ -6,17 +6,15 @@ const nb = document.getElementById('Nav');
 const msg = document.getElementById("Message")
 const ip = document.getElementById('inputid');
 const mc = document.getElementById('learnmc');
-
+const yt = document.getElementById('ytButton');
 
 const c = "1000"
-
 
 document.addEventListener('keydown', (event) => {
     if (event.key === 'Shift' && ip.value === c) {
         nb.style.display = 'flex';
         mc.style.display = "none";
         msg.textContent = "Thanks for being a member! Navigate through the site with the menu above."
-        document.cookie = "Authenticated=true";
     } else {
         nb.style.display = 'none';
         mc.style.display = "block";
@@ -33,7 +31,20 @@ mB.addEventListener('click', function () {
     iframe.style.width = '100%';
     iframe.style.height = '100%';
     iframe.style.margin = '0';
-    iframe.src = ip.value + "file:///home/chronos/u-a6403f850ebd9c9ddd0e13b353dd2da93a78c2d0/MyFiles/Page/unrestricted.html";
+    iframe.src = "C:/Users/Gav/Desktop/! Projects/IMDB/index.html";
+    win.document.body.appendChild(iframe);
+});
+
+yt.addEventListener('click', function () {
+    win = window.open();
+    win.document.body.style.margin = '0';
+    win.document.body.style.height = '100vh';
+    var iframe = win.document.createElement('iframe');
+    iframe.style.border = 'none';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.margin = '0';
+    iframe.src = "https://108.181.43.67/results?search_query=cat"; /* https://www.youtubeunblocked.live/ */
     win.document.body.appendChild(iframe);
 });
 
